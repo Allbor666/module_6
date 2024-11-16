0fdb1d414a27c6994344300ca021e0f6cfa65c6c
Allbor666 <allbor347@gmail.com>
2024-11-16 20:17:03 +0700

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -1,41 +1,44 @@
 
 
 class Animal:
+    alive = True  # Живое (классовый атрибут)
+    fed = False    # Накормленное (классовый атрибут)
+
     def __init__(self, name):
-        self.alive = True  # Живое
-        self.fed = False   # Накормленное
         self.name = name
+
+    def eat(self, food):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
 
 class Plant:
     def __init__(self, name):
-        self.edible = False  # Съедобность
+        self.edible = False  # Съедобность (по умолчанию)
         self.name = name
 
+
 class Mammal(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass  # Млекопитающие используют метод eat из Animal
+
 
 class Predator(Animal):
-    def eat(self, food):
-        if food.edible:
-            print(f"{self.name} съел {food.name}")
-            self.fed = True
-        else:
-            print(f"{self.name} не стал есть {food.name}")
-            self.alive = False
+    pass  # Хищники используют метод eat из Animal
+
 
 class Flower(Plant):
-    pass  # Цветы не съедобные
+    pass  # Цветы являются несъедобными (по умолчанию)
+
 
 class Fruit(Plant):
     def __init__(self, name):
         super().__init__(name)
         self.edible = True  # Фрукты съедобные
+
 
 # Создание объектов классов
 a1 = Predator('Волк с Уолл-Стрит')
@@ -44,12 +47,17 @@ p1 = Flower('Цветик семицветик')
 p2 = Fruit('Заводной апельсин')
 
 # Проверка состояния объектов и выполнение методов
-print(a1.name)  # Волк с Уолл-Стрит
-print(p1.name)  # Цветик семицветик
+print(a1.name) # Волк с Уолл-Стрит
+print(p1.name) # Цветик семицветик
 
-print(a1.alive)  # True
-print(a2.fed)    # False
-a1.eat(p1)       # Волк с Уолл-Стрит не стал есть Цветик семицветик
-a2.eat(p2)       # Хатико съел Заводной апельсин
-print(a1.alive)  # False
-print(a2.fed)    # True
+# Изначальное состояние
+print(a1.alive) # True
+print(a2.fed) # False
+
+# Животные пытаются поесть
+a1.eat(p1)      # Волк с Уолл-Стрит не стал есть Цветик семицветик
+a2.eat(p2)      # Хатико съел Заводной апельсин
+
+# Проверка состояния после еды
+print(a1.fed)    # False (так как не поел)
+print(a2.fed)    # True (так как поел)
